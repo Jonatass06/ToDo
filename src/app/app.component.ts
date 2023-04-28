ad1a948bca1591c86992df225ceb8bee20126a6b
JONATAS JACKSON GONÇALVES <jonatas_goncalves@estudante.sesisenai.org.br>
2023-04-27 21:55:51 -0300

    Ehhhh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
 
+interface Tarefa{
+  texto:string,
+  categoria:string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'todo-app';
+
+  mostraInput: boolean = true;
+  
+  Tarefas:Tarefa[] = [];
+
+  tarefa:Tarefa ={
+    texto: "",
+    categoria:""
+  }
+  CadastrarTarefa():void{
+    const TarefaInserida: Tarefa = {
+      texto: this.tarefa.texto,
+      categoria: this.tarefa.categoria
+    }
+    this.Tarefas.push(TarefaInserida);
+    this.tarefa.texto = null;
+    localStorage.setItem("listaTarefas", JSON.stringify(this.Tarefas));
+  }
+  Del(item){
+    const index = this.Tarefas.indexOf(item);
+    this.Tarefas.splice(index, 1);
+    localStorage.setItem("listaTarefas", JSON.stringify(this.Tarefas));
+
+ }
+ defineCategoria(indice, selected):void{
+    this.Tarefas[indice].categoria = selected;
+ }
+
 }
