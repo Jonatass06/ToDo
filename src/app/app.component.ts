cd83d49195aabdfa38fba5f18c0a9529a028f20c
JONATAS JACKSON GONÇALVES <jonatas_goncalves@estudante.sesisenai.org.br>
2023-05-04 21:51:27 -0300

    Separa as tarefas por categoria

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
-interface Tarefa{
-  texto:string,
-  categoria:string,
-  titulo:string
+interface Tarefa {
+  texto: string,
+  categoria: string,
+  titulo: string
 }
 
 @Component({
@@ -14,44 +14,60 @@ interface Tarefa{
 
 export class AppComponent {
 
-  defineLista():Tarefa[]{
-    let Tarefas:Tarefa[] = [];
-    if(localStorage.getItem("listaTarefas") != null){
-       Tarefas = JSON.parse(localStorage.getItem("listaTarefas"));
+  defineLista(): Tarefa[] {
+    let Tarefas: Tarefa[] = [];
+    if (localStorage.getItem("listaTarefas") != null) {
+      Tarefas = JSON.parse(localStorage.getItem("listaTarefas"));
     }
     return Tarefas;
   }
-  Tarefas:Tarefa[] = this.defineLista();
+  Tarefas: Tarefa[] = this.defineLista();
 
   title = 'todo-app';
-  
-  tarefa:Tarefa ={
-    texto: null,
-    categoria:null,
-    titulo:null
-  }
-  CadastrarTarefa():void{
-    if(this.tarefa.categoria == null){
-      this.tarefa.categoria = "To Do"
-    }
-    const TarefaInserida: Tarefa = {
-      texto: this.tarefa.texto,
-      categoria: this.tarefa.categoria,
-      titulo: this.tarefa.titulo
-    }
-    this.Tarefas.push(TarefaInserida);
-    this.tarefa.texto = null;
-    this.tarefa.categoria = null;
-    this.tarefa.titulo = null;
-    localStorage.setItem("listaTarefas", JSON.stringify(this.Tarefas));
-  }
-  Del(item:any){
-    const index = this.Tarefas.indexOf(item);
-    this.Tarefas.splice(index, 1);
-    localStorage.setItem("listaTarefas", JSON.stringify(this.Tarefas));
 
- }
- mudaCat():void{
-  localStorage.setItem("listaTarefas", JSON.stringify(this.Tarefas));
- }
+  tarefa: Tarefa = {
+    texto: null,
+    categoria: null,
+    titulo: null
+  }
+
+  corTexto = 'Black';
+  tema = "White"
+
+  mudaTema():void{
+    if(this.tema == "White"){
+      this.corTexto = 'Black';
+    } else{
+      this.corTexto = 'White';
+    }
+  }
+
+  CadastrarTarefa(): void {
+    if ((this.tarefa.titulo == null && this.tarefa.texto == null) ||
+    (this.tarefa.titulo == null && this.tarefa.texto == "")||
+    (this.tarefa.titulo == "" && this.tarefa.texto == null)||
+    (this.tarefa.titulo == "" && this.tarefa.texto == "")) {}
+    else{
+      if (this.tarefa.categoria == null) {
+        this.tarefa.categoria = "To Do"
+      }
+      const TarefaInserida: Tarefa = {
+        texto: this.tarefa.texto,
+        categoria: this.tarefa.categoria,
+        titulo: this.tarefa.titulo
+      }
+      this.Tarefas.push(TarefaInserida);
+      this.tarefa.texto = null;
+      this.tarefa.categoria = null;
+      this.tarefa.titulo = null;
+      localStorage.setItem("listaTarefas", JSON.stringify(this.Tarefas));
+    }
+  }
+  Del(indice: number) {
+    this.Tarefas.splice(indice, 1);
+    localStorage.setItem("listaTarefas", JSON.stringify(this.Tarefas));
+  }
+  mudaCat(): void {
+    localStorage.setItem("listaTarefas", JSON.stringify(this.Tarefas));
+  }
 }
